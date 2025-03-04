81dcddecb6a0851c33920ebd00bd6daffa10bbe3
Rotomeca <delphin.tommy@gmail.com>
2025-03-04 17:09:54 +0100

    fix(.d.ts)

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
 import { EMPTY_STRING } from './lib/constants';
 import Random from './lib/random';
-export = {isNullOrUndefined, Random, EMPTY_STRING, isArrayLike};
 declare module "@rotomeca/utils";
-function isNullOrUndefined(item: any): boolean;
+export declare function isNullOrUndefined(item: any): boolean;
 /**
  * Vérifie si une variable est un tableau ou quelque chose qui y ressemble
  * @param {any} item Element à vérifier
  * @returns {bool} true si l'élément est un tableau ou quelque chose qui y ressemble
  */
-function isArrayLike(item: any): boolean;
+export declare function isArrayLike(item: any): boolean;
+export { Random, EMPTY_STRING };
 
